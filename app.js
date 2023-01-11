899bf73d42b9cefd5c75e31a04a18db87068abe5
Sreenivasan <sreenivasansudharsan49@gmail.com>
2023-01-11 18:42:54 +0530

    *Commentary on App.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('*', (req, res)=> {
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build'))); //This uses the static files created using react
 app.use(express.json());
 app.use(helmet());
 app.use(xss());

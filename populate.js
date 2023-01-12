2b1b82df98fc8433dec2ce674f59c31f49ee3b2d
Sreenivasan <sreenivasansudharsan49@gmail.com>
2023-01-12 11:43:26 +0530

    Added Populate

---
+++ populate.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+
+const mockData = require('./MOCK_DATA (1).json')
+
+const Job = require('./models/Job')
+const connectDB = require('./db/connect');
+
+
+const start = async()=> {
+   try {
+        await connectDB(process.env.MONGO_URI)
+
+        await Job.create(mockData);
+        console.log('Success!!')
+        process.exit(0);
+
+   } catch (error) {
+    console.log(error);
+    process.exit(1)
+   } 
+}
+
+start()
